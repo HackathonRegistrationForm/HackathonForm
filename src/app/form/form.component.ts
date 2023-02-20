60a425070380a757e3ef1b1ff0e54060f8417574
Umavengalaraju <umavengalaraju20@gmail.com>
2023-02-20 12:09:09 +0530

    first commit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { subscribeOn } from 'rxjs';
 import Swal from 'sweetalert2';
 
 @Component({
